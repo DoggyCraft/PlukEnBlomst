af52ae7727e4913556739ff3ea51adf3b3a66a8c
Amqlie <kawaiitubs@gmail.com>
2019-08-27 21:30:36 +0200

    Deleted import org.bukkit.Server

--- src/com/amqlie/plukenblomst/PlukEnBlomst.java
+++ src/com/amqlie/plukenblomst/PlukEnBlomst.java
@@ -7,7 +7,6 @@ import java.util.UUID;
 import java.util.logging.Logger;
 
 import org.bukkit.ChatColor;
-import org.bukkit.Server;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Entity;
